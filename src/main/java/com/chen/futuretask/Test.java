d34f679f78a40030c9335224d89d51c48348a3b6
chenyong <c1097210504@qq.com>
2017-08-16 14:38:08 +0800

    添加readme文件

--- src/main/java/com/chen/futuretask/Test.java
+++ src/main/java/com/chen/futuretask/Test.java
@@ -5,11 +5,23 @@ import java.util.concurrent.*;
 /**
  * Created by chenyong on 2017-08-16.
  * 线程池需要手动关闭
+ * futureTask 的get方法会一直等待任务执行完毕才输出
+ * 也就是说他是一个阻塞的方法
+ *
+ *
+ * FutureTask 和Future是有区别的
+ * FutureTask 需要一个Callable来传入构造函数
+ * 而Future则不需要
+ * 因为在线程池执行任务的时候 调用的方法也不一样
+ * FutureTask被使用的时候 调用的是线程池的execute方法
+ * 而Future被使用时，调用的是线程池的submit放方法
+ * 返回值是future
+ *
  */
 public class Test {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException, TimeoutException {
-        MyCallable callable = new MyCallable(1000);
+        MyCallable callable = new MyCallable(10000);
         MyCallable callable2 = new MyCallable(2000);
 
         FutureTask<String> futureTask1 = new FutureTask<String>(callable);
@@ -26,7 +38,13 @@ public class Test {
             }
 
             if(!futureTask1.isDone()){
+                /**
+                 * get方法会一直等待任务执行完毕 才执行下面的 代码
+                 * 阻塞的
+                 */
+                System.out.println("等在执行输出");
                 System.out.println("future task1 output = "+futureTask1.get());
+                System.out.println("执行完毕");
             }
 
             System.out.println("Waiting for FutureTask2 to complete");
